7f12c436a220904404f93c885855cc78afc6ca93
woooyoung <axdsw121@gmail.com>
2025-03-04 09:50:37 +0900

    커밋 테스트 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,7 @@
 package org.example;
 
-// 문제 : `안녕하세요.`를 10번 출력해주세요.
-
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
-        System.out.println(1); // 실행순서 1 : 숫자는 " 로 감싸지 않아도 됩니다.
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("사");
-
-        System.out.print("안녕\n안녕\n안녕");
     }
 }
